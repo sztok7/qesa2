85bea3da745aab71a4dd98d2a6e64044800cb493
sztok7 <59089574+sztok7@users.noreply.github.com>
2020-12-04 13:25:41 +0100

    Adds hamburger menu, gallery fix (#6)
    * Ses
    
    * Ses no.2
    
    * Prc milojko
    
    * Hihihu
    
    * Fixed gallery, hamburger menu and centered content
    
    Co-authored-by: Filip Štoković <filip.stokovic@bulbtech.com>

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+function showMenu() {
+  $('#big-menu').addClass('active');
+}
+
+function closeMenu() {
+  $('#big-menu').removeClass('active');
+}
+
+function setLocation(location) {
+  window.location = location;
+}
